718e42b82bf879b5a0d5627978493d8a5e68e04e
Todgerel <112902040+Todgerel@users.noreply.github.com>
2022-09-26 10:07:40 -0500

    lab13

--- lab13/q1/index.js
+++ lab13/q1/index.js
@@ -1,13 +1,18 @@
-const express = require('express'); 
+const express = require('express');
 const app = express();
 const path = require('path');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "view"));
+
+const date = new Date();
+const hour = date.getHours();
+
+app.use('/css', express.static(path.join(__dirname, 'css', (hour > 6 && hour < 18) ? 'day.css':'night.css')));
+
 app.get('/', (req, res) => {
-    const date = new Date();
     res.render("index", {
-    time: date.toTimeString(),
+        time: date.toTimeString()
     });
 });
 
